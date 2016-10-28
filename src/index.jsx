4a2055475a8875dee5646f53069ff67f1b91798e
AJ Meyghani <amin.meyghani@aofl.com>
2016-10-28 15:19:49 -0700

    add example, parent callback.

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-ReactDOM.render(<h1>Hello React!!</h1>, document.getElementById('app'));
+
+var App = React.createClass({
+  signupResult() {
+    console.log('signing up ....');
+  },
+  render() {
+    return (
+    <div>
+      <Signup onSignup={this.signupResult} />
+    </div>
+    );
+  }
+});
+
+var Signup = React.createClass({
+  login() {
+    console.log('loging in');
+    setTimeout(() => {
+      this.props.onSignup();
+    }, 2000);
+  },
+  render() {
+    return (
+    <div>
+      <p>Sign up</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic fugit voluptate, distinctio impedit temporibus corrupti placeat, tenetur saepe voluptatibus enim, officiis explicabo ea praesentium modi itaque omnis voluptatem. At, et?</p>
+      <button onClick={this.login}>Sign up</button>
+    </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
