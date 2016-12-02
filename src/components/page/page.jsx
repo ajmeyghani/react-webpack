ace90fc300b4fe436631eae126935add4b6ce8ca
Amin Meyghani <meyghania@gmail.com>
2016-12-01 19:04:09 -0800

    retain line numbers.

--- src/components/page/page.jsx
+++ src/components/page/page.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const pageTpl = require('page/page.tpl');
 require('page/page.css');
 export default React.createClass({
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react'],
+          retainLines: 'true',
           plugins: ['add-module-exports']
         }
       },
