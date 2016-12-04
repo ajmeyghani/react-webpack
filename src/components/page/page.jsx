d358aa5b230b25ff156e3a3f703a88996a34c63a
Amin Meyghani <meyghania@gmail.com>
2016-12-04 14:54:20 -0800

    fine tune source mapping.

--- src/components/page/page.jsx
+++ src/components/page/page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 const pageTpl = require('page/page.tpl');
 require('page/page.css');
 export default React.createClass({
+
   render() {
     return pageTpl;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve('./dist')
   },
-  devtool: "source-map",
+  devtool: "cheap-module-source-map",
   module: {
     loaders: [
       {
