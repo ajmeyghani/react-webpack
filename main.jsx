5c7391a6a45eeb642fde6583540226646e857ff3
Amin Meyghani <meyghania@gmail.com>
2016-12-06 18:45:15 -0800

    sub children.

--- main.jsx
+++ main.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from 'page/page';
+import DetailContainer from 'detail-container/detail-container';
+
+const someItem = {
+  id: 1, name: 'tom'
+};
 
 const App = React.createClass({
   render() {
     return (
       <div>
         <Page />
+        <DetailContainer item={someItem}>
+          <p>This is the first child</p>
+          <p>This is the second one</p>
+        </DetailContainer>
       </div>
       );
     }
---
+++ src/components/detail-container/detail-container.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// const detailContainer = require('detail-container/detail-container.jsx.html');
+export default React.createClass({
+  render() {
+    return require('detail-container/detail-container.jsx.html')(this)
+  }
+});
